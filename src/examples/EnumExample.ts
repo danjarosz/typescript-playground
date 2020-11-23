d72589e634370b0f68217db7c588ee7a73c2ace9
danjarosz <jarosz.daniel@protonmail.com>
2020-11-24 00:31:58 +0100

    prevent render error because of examples

--- src/examples/EnumExample.ts
+++ src/examples/EnumExample.ts
@@ -1,23 +1,27 @@
-enum Example {
-    USER,
-    MODERATOR,
-    ADMIN,
-    GOD = 12,
-    TEST,
-}
+// // Enum example
+// enum Example {
+//     USER,
+//     MODERATOR,
+//     ADMIN,
+//     GOD = 12,
+//     TEST,
+// }
 
-type UserTypes = 0 | 1 | 2;
+// type UserTypes = 0 | 1 | 2;
 
-interface User {
-    type: UserTypes
-}
+// interface User {
+//     type: UserTypes
+// }
 
-const user: User = {
-    type: 1
-}
+// const user: User = {
+//     type: 1
+// }
 
-if (user.type === Example.USER) {
-    console.log("Uzytkownik")
-} else if (user.type === Example.MODERATOR) {
-    console.log("Moderator")
-}
+// if (user.type === Example.USER) {
+//     console.log("Uzytkownik")
+// } else if (user.type === Example.MODERATOR) {
+//     console.log("Moderator")
+// }
+
+const fn = () => {}
+export default fn;
--- src/examples/GenericTypeExample.ts
+++ src/examples/GenericTypeExample.ts
@@ -1,20 +1,23 @@
 // Array jako typ generyczny
-interface Point {
-    x: number;
-    y: number;
-}
+// interface Point {
+//     x: number;
+//     y: number;
+// }
 
-const p1: Point = {
-    x: 4,
-    y: 5
-}
+// const p1: Point = {
+//     x: 4,
+//     y: 5
+// }
 
-const example: Array<Point> = [];
+// const example: Array<Point> = [];
 
-example.push(p1)
+// example.push(p1)
 
-//Funkcja jako typ generyczny
-// T zastępuje dowolny typ, chodzi o idee.
-const exampleFunction = <T>(element: T) => element;
+// //Funkcja jako typ generyczny
+// // T zastępuje dowolny typ, chodzi o idee.
+// const exampleFunction = <T>(element: T) => element;
 
-const test1 = exampleFunction(p1);
+// const test1 = exampleFunction(p1);
+
+const fn = () => {}
+export default fn;
