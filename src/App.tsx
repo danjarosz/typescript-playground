a6b7a26cd2dc17aa571daaa6bff2583d373cbb96
danjarosz <jarosz.daniel@protonmail.com>
2020-11-23 20:06:56 +0100

    feat: functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //types
   let address = "ul. Porzeczkowa"
   let numberOfAge: string;
   let something: number | string;
   address = "5";
-  numberOfAge = "13"
-  something = 44
+  numberOfAge = "13";
+  something = 44;
+
+  // functions
+  let x = 5;
+  let y = 10;
+
+  const add = (x: number, y: number): number => x + y;
+  console.log(add(x, y));
+
 
   return (
     <div className="App">
