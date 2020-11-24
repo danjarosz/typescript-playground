3e2b55124062b3c17bbda2bb1b5af7e8774ec027
danjarosz <jarosz.daniel@protonmail.com>
2020-11-24 19:50:18 +0100

    refactor: extract Types.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Example from "./examples/ClassExample";
 
 
 function App() {
-  // //types
-  // let address = "ul. Porzeczkowa"
-  // let numberOfAge: string;
-  // let something: number | string;
-  // address = "5";
-  // numberOfAge = "13";
-  // something = 44;
-
-  // // functions
-  // let x = 5;
-  // let y = 10;
-
-  // const add = (x: number, y: number): number => x + y;
-  // console.log(add(x, y));
-
-  // //classes
-  // const ex = new Example(123, "Daniel", "Jarosz");
-  // console.log(ex, ex.id, ex.surname);
-  // ex.showId();
-  // console.log("Generic: ", ex.generic)
-
-
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/examples/Types.ts
@@ -0,0 +1,25 @@
+// import Example from "./examples/ClassExample";
+
+// //types
+  // let address = "ul. Porzeczkowa"
+  // let numberOfAge: string;
+  // let something: number | string;
+  // address = "5";
+  // numberOfAge = "13";
+  // something = 44;
+
+  // // functions
+  // let x = 5;
+  // let y = 10;
+
+  // const add = (x: number, y: number): number => x + y;
+  // console.log(add(x, y));
+
+  // //classes
+  // const ex = new Example(123, "Daniel", "Jarosz");
+  // console.log(ex, ex.id, ex.surname);
+  // ex.showId();
+  // console.log("Generic: ", ex.generic)
+
+  const fn = () => {}
+export default fn;
